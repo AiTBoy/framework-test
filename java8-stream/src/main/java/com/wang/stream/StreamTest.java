897ab3080d710ad30481ee2aa745de04c31e55b5
lue.wang <2441704893@qq.com>
2021-04-27 21:54:00 +0800

    Feature:0001;Subject:first commit;Reviewer:lue.wang;

---
+++ java8-stream/src/main/java/com/wang/stream/StreamTest.java
@@ -0,0 +1,24 @@
+package com.wang.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamTest {
+
+    @Test
+    public void test0(){
+        List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4, 5, 6);
+
+        // 过滤
+        Stream<Integer> integerStream = numbers.stream().filter(num -> num > 3).distinct();
+        System.out.println(integerStream);
+
+        // 映射
+            numbers.stream().filter(num -> num > 3).map(i -> numbers.get(i)).collect(Collectors.toList()    );
+    }
+
+}
